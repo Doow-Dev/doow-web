c30c3ab00734aaed956ac5b62f7c37a72270de17
Philix27 <philixbob@gmail.com>
2023-03-16 14:44:46 +0100

    footer framework set

--- src/views/home/footer/footer.comp.tsx
+++ src/views/home/footer/footer.comp.tsx
@@ -2,25 +2,41 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "./footer.module.scss";
-import { FaTimes, FaBars } from "react-icons/fa";
 import Image from "next/image";
 
 export default function FooterHome() {
-  const router = useRouter();
-  const _path = router.pathname.toString();
-  const [user, setUser] = useState(null);
-  const show = "show";
-  const [showNav, setShowNav] = useState(false);
-  // console.log("Path -", _path);
   return (
-    <>
-      <div className={styles.container}>
-        <label className={styles.brand}>
-          <Link href="/" className={styles.brand}>
-            DOOW
-          </Link>
-        </label>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h4 className={styles.brand}> DOOW</h4>
+        <div className={styles.linkList}>
+          <ul>
+            <li>Links</li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          <ul>
+            <li>Navigation</li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          <ul>
+            <li>Products</li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={styles.shortText}>
+          We are the choosen generation, called to show forth his excellency.
+        </div>
+        <div className={styles.copywright}>
+          <p>© copyright All rights reserved</p>
+          <p>Powered by Hexelan</p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
