d3de761ecd6f0a6fc0b3ef130148a77f1dc85a7e
codeblock1 <onwuahakenneth@gmail.com>
2023-03-18 17:01:12 +0100

    add sidebar component

--- src/comps/sidebar/sidebar.tsx
+++ src/comps/sidebar/sidebar.tsx
@@ -1,9 +1,14 @@
 import styles from "./sidebar.module.scss";
 import Link from "next/link";
 import { logout, useAuthDispatch, useAuthState } from "../../context/auth";
-import { sidebarItems } from "./sidebarItems";
+import { sideBarItems } from "./sidebar-items";
+import { TfiAngleUp, TfiAngleDown } from "react-icons/tfi";
+import { useState } from "react";
+import Image from "next/image";
 
 export const SideBar = () => {
+  const [showLinks, setShowLinks] = useState<boolean>(false);
+  const [activeLink, setActiveLink] = useState<number | null>(null);
   const dispatch = useAuthDispatch();
   const handleLogout = async () => {
     await logout(dispatch);
@@ -11,29 +16,62 @@ export const SideBar = () => {
 
   const data = useAuthState();
 
+  const toggleShowLinks = (i: number) => {
+    if (i === activeLink) {
+      setShowLinks((prev) => !prev);
+    } else {
+      setActiveLink(i);
+      setShowLinks(true);
+    }
+  };
+
   return (
-    <div className={styles.sidebar}>
+    <div className={`${styles.sidebar_menu} ${styles.active}`}>
       <Link href="/">
         <h3>DOOW</h3>
       </Link>
-      <ul>
-        {sidebarItems.map((item, i) => {
-          // if (item.agentType.includes(data?.me?.agent_type)) {
-          return (
-            <Link href={item.link} key={i}>
-              <li
-                className={styles.styles}
-                key={i}
-                onClick={item.title === "Logout" ? handleLogout : () => {}}
-              >
-                <span>{item.icon}</span>
-                {item.title}
-              </li>
-            </Link>
-          );
-          // }
-        })}
-      </ul>
+      <div className={styles.sidebar}>
+        <div className={styles.links}>
+          {sideBarItems.map((item, i) => (
+            <div
+              className={`${styles.link} ${
+                showLinks && activeLink === i ? styles.active : ""
+              }`}
+              key={i}
+            >
+              <Link href={"#"} onClick={() => toggleShowLinks(i)}>
+                {item.icon}
+                <p>{item.title}</p>
+                <TfiAngleUp />
+              </Link>
+              <div className={styles.sub_menu}>
+                {item.subLinks.map((link, idx) => (
+                  <Link key={idx} href={link.link}>
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+          {/* add active class */}
+        </div>
+        {/* doow logo section */}
+        <div className={styles.logoSection}>
+          <Image
+            src={"/assets/icons/doowlogo-circle.png"}
+            width={40}
+            height={40}
+            alt={"doow-logo"}
+          />
+          <div className={styles.logoInfo}>
+            <div>
+              <p>Doow Inc.</p>
+              <TfiAngleDown />
+            </div>
+            <span>Abisoye Tolani</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
