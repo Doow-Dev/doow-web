c4b6254dc1fd2f15823f6c4e9c25855af7879b51
codeblock1 <onwuahakenneth@gmail.com>
2023-03-20 01:12:49 +0100

    fix build error

--- src/views/profile/profile.view.tsx
+++ src/views/profile/profile.view.tsx
@@ -1,7 +1,6 @@
 import NavBar from "../../comps/navbar/navbar.comp";
 import { SideBar } from "../../comps/sidebar/sidebar";
 import { useAuthState } from "../../context/auth";
-import LandingNavBar from "../Landing/navbar/navbar.comp";
 import styles from "./profile.module.scss";
 
 export const ProfileView = () => {
@@ -14,7 +13,6 @@ export const ProfileView = () => {
   return (
     <>
       <SideBar />
-      <LandingNavBar />
       <div className={styles.container}>
         <div className={styles.contents}>
           <p>Content here</p>
