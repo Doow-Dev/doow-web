46c46b291728f7aa74da826d252a62c55379c4b2
Danny5295 <daniel.ejeh@doow.co>
2025-02-24 06:10:38 +0100

    add landing page layout

---
+++ src/components/layout/Landing.tsx
@@ -0,0 +1,21 @@
+import { BankingSection } from "../custom/banking/BankingSection";
+import { ProductDemoSection } from "../custom/demo/ProductDemoSection";
+import { FAQSection } from "../custom/faq/FAQSection";
+import { HeroSection } from "../custom/hero/HeroSection";
+import { IntegrationsSection } from "../custom/integrations/IntegrationsSection";
+import { SassIntelligenceSection } from "../custom/sassIntelligence/SaasIntelligence";
+import { WaitListBannerSection } from "../custom/waitlistBanner/WaitListBannerSection";
+
+export const Landing: React.FC = () => {
+    return (
+        <>
+            <HeroSection/>
+            <ProductDemoSection/>
+            <IntegrationsSection/>
+            <SassIntelligenceSection/>
+            <BankingSection/>
+            <WaitListBannerSection/>
+            <FAQSection/>
+        </>
+    )
+};
