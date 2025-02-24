b4e2eb0a645bcd7262f4a4225ce86efb27416c09
Danny5295 <daniel.ejeh@doow.co>
2025-02-24 05:33:56 +0100

    batch delete old project files and folders

--- pages/home/unsubscribe.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import UnsubscribeView from "../../src/views/unsubscribe/unsubscribe.view";
-
-export default function UnsubscribePage() {
-  return <UnsubscribeView />;
-}
--- src/styles/themes/globalstyles.ts
+++
@@ -1,9 +0,0 @@
-export const globalStyles = {
-  colors: {
-    primary: "#07c524",
-    backgroundColor: "#eff5ef",
-    black800: "#212121",
-    success: "#27AE60",
-    fail: "#EB5757",
-  },
-};
