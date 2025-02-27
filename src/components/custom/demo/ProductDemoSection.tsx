586d8c9286c739de35c0124133852e5845dd4734
Danny5295 <daniel.ejeh@doow.co>
2025-02-27 16:38:29 +0100

    update bg style

--- src/components/custom/demo/ProductDemoSection.tsx
+++ src/components/custom/demo/ProductDemoSection.tsx
@@ -5,8 +5,10 @@ import Image from "next/image"
 
 export const ProductDemoSection = () => {
   return (
-      <section className="container flow-root section-spacing px-3 bg-white">
-          <div className="text-center mb-8 space-y-4 max-w-xl md:max-w-2xl mx-auto"><h2 className="text-sub-heading text-doow_zinc capitalize">Eliminate wasted software spend, prevent surprise renewals, and streamline recurring vendor payments—all from a single dashboard.</h2></div>
+      <section className="container flow-root section-spacing px-3 bg-[#003138]">
+          <div className="text-center mb-8 max-w-xl md:max-w-3xl mx-auto text-pretty">
+            <h2 className="text-sub-heading text-doow_offwhite capitalize">Eliminate wasted software spend, prevent surprise renewals, and streamline recurring vendor payments—all from a single dashboard.</h2>
+          </div>
           <div className="rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4">
             <Image
               src={AppImages.demos.dashboard}
