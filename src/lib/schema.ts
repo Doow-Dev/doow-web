52dd8f9b2312126e4ec437aabb1eb885b8029248
Danny5295 <daniel.ejeh@doow.co>
2025-02-27 16:34:26 +0100

    add validation for work emails

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,11 +1,31 @@
 import { z } from "zod"
 
+const workMailSchema = z
+  .string()
+  .email("Please enter a valid email")
+  .refine(
+    (email) => {
+      const foundDomain = personalEmailDomains.find((domain) =>
+        email.toLowerCase().endsWith(domain)
+      );
+      return !foundDomain; // Returns false if a personal domain is found
+    },
+    (email) => {
+      const foundDomain = personalEmailDomains.find((domain) =>
+        email.toLowerCase().endsWith(domain)
+      );
+      return {
+        message: `Please enter a valid work email address, not (${foundDomain})`,
+      };
+    }
+  );
+
 export const emailSchema = z.object({
-  email: z.string().email("Please enter a valid work email address"),
+  email: workMailSchema
 })
 
 export const waitListSchema = z.object({
-    email: z.string().email("Please enter a valid work email address"),
+    email: workMailSchema,
     firstName: z.string().min(2, "Name must be at least 2 characters"),
     lastName: z.string().min(2, "Name must be at least 2 characters"),
     company: z.string().min(1, "Company name is required"),
@@ -14,3 +34,28 @@ export const waitListSchema = z.object({
 
 export type EmailFormData = z.infer<typeof emailSchema>
 export type WaitListFormData = z.infer<typeof waitListSchema>
+
+
+// List of common personal email domains
+const personalEmailDomains = [
+  "@gmail.com",
+  "@outlook.com",
+  "@apple.com",
+  "@me.com",
+  "@icloud.com",
+  "@mac.com",
+  "@qq.com",
+  "@orange.fe",
+  "@wed.de",
+  "@yahoo.com",
+  "@myyahoo.com",
+  "@yahoo.co.uk",
+  "@yahoo.fr",
+  "@aol.com",
+  "@icloudmail.com",
+  "@live.com",
+  "@hotmail.com",
+  "@msn.com",
+  "@yandex.ru",
+  "@googlemail.com",
+];
