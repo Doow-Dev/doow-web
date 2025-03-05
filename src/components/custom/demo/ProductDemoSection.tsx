6bc4fed9214e78ad14f149451a81019c3fd341b0
Danny5295 <daniel.ejeh@doow.co>
2025-03-05 06:26:23 +0100

    update demo bg and styling

--- src/components/custom/demo/ProductDemoSection.tsx
+++ src/components/custom/demo/ProductDemoSection.tsx
@@ -1,3 +1,4 @@
+import { MaxWidthWrapper } from "@/components/ui/max-width-wrapper"
 import { AppImages } from "@/lib/config/app-images"
 // import { maxHeaderSize } from "http"
 import Image from "next/image"
@@ -5,20 +6,23 @@ import Image from "next/image"
 
 export const ProductDemoSection = () => {
   return (
-      <section className="container flow-root section-spacing px-3 bg-[#003138]">
+      <section className="h-full w-full bg-[#003138]">
+        <MaxWidthWrapper className="section-spacing flow-root ">
+          {/* TODO: use react warp balancer for better headings and a resuable ui component for headings */}
           <div className="text-center mb-8 max-w-xl md:max-w-3xl mx-auto text-pretty">
             <h2 className="text-sub-heading text-doow_offwhite capitalize">Eliminate wasted software spend, prevent surprise renewals, and streamline recurring vendor payments—all from a single dashboard.</h2>
           </div>
-          <div className="rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl lg:p-4">
+          <div className="rounded-xl bg-white/5 backdrop-blur-sm p-2 ring-1 ring-inset ring-white/20 lg:rounded-2xl lg:p-4">
             <Image
               src={AppImages.demos.dashboard}
-              alt="s"
+              alt="Dashboard preview"
               width={1364}
               height={866}
               quality={100}
               className="rounded-md bg-white  shadow-2xl ring-1 ring-gray-900/10"
             />
           </div>
+        </MaxWidthWrapper>
       </section>
   )
 }
