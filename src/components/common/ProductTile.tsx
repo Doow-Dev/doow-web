7cef4742380fa07ac8cf11916885cd5f834a3155
Danny5295 <daniel.ejeh@doow.co>
2025-03-05 06:28:30 +0100

    update styling to remove shadow

--- src/components/common/ProductTile.tsx
+++ src/components/common/ProductTile.tsx
@@ -11,7 +11,7 @@ interface BankingTileProps{
 
 export const ProductTile = ({title, subtitle, children, className}: BankingTileProps) => {
   return (
-    <Card className={cn("bg-doow_card hover:shadow-lg", className)}>
+    <Card className={cn("bg-doow_card", className)}>
         <CardHeader>
             <CardTitle>{title}</CardTitle>
             {subtitle && 
