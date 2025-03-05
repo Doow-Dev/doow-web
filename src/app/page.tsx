10a6979cdcad760d0d658ce1c58293bd55f55f9a
Danny5295 <daniel.ejeh@doow.co>
2025-03-05 06:22:37 +0100

    update home page composition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { Footer } from "@/components/layout/footer/Footer";
 import { Header } from "@/components/layout/header/Header";
 import { Landing } from "@/components/layout/Landing";
+import { NavigationMenuDemo } from "@/components/layout/nav/Nav-Menu";
 
 export default function Home() {
   return (
-      <div>
+      <>
         <Header/>
         <main>
           <Landing/>
         </main>
         <Footer/>
-      </div>
+      </>
   );
 }
